93e3a8b7e1597eabf02ea79ff54e132bbc4238b1
Daniel Santos Rodrigues <danielrodrigues.dev@gmail.com>
2026-07-28 17:50:25 -0300

    first commit

---
+++ fundamental/src/App.jsx
@@ -0,0 +1,16 @@
+// Components
+import FirstComponent from "./components/FirstComponent";
+
+// Styles
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
